04364ddf90817f362a5c1aa0df16c66a96328d53
HYE <342333507@tdsb.ca>
2026-02-19 11:34:51 -0500

    Added some filler code to node.js

--- node.js
+++ node.js
@@ -1,5 +1,11 @@
 // Checking node installed or not.
 // node --version
 // npm --version
-
 // https://www.w3schools.com/nodejs/nodejs_get_started.asp
+
+let http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);
+
